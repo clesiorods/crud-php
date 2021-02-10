a89acb8885767b790900b84d771f026724840a65
clesiorods <clesiorodrigues96@gmail.com>
2021-02-10 18:37:58 -0300

    1.1.2

--- includes/ficha.php
+++ includes/ficha.php
@@ -1,14 +1,13 @@
 <main>
 
-
-
 <h2 class="mt-4"><?=TITLE?></h2>
+<h3 class="text-primary">Nome da Empresa</h3>
 
+<div class="d-flex flex-row-reverse bg-white">
+    <h3 class="text-success">R$ 2.200,00</h3>
+</div>
 
-
-
-
-
+<p class="mb-5 pb-5 pt-5">skjhfsahjvjvkljadfnv eak nkjearfkjm vk aek keajrn kjneargjk naekjrn jkerna njkngkjnae gjneargjkn jsdv  fv as v sd anfdefvjadefvd vjndvge vervsj sv vaejglerjgn er er vrfgergregag  gg ertg grteg erg gertge rgkngn g</p>
 
 
 
